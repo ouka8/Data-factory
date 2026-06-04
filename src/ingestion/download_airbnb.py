67d75e8129f4fe740dc8195a155f27b812ce6246
Amal <amalamraouiest@gmail.com>
2026-06-04 10:58:16 +0100

    Refactor download function in download_airbnb.py
    Updated print statements and improved file listing logic.

--- src/ingestion/download_airbnb.py
+++ src/ingestion/download_airbnb.py
@@ -1,30 +1,27 @@
 import os
 from kaggle.api.kaggle_api_extended import KaggleApi
-import pandas as pd
 
 DATASET = "dgomonov/new-york-city-airbnb-open-data"
 
 def download():
-    print("Starting Kaggle download...")
+    print("Starting Kaggle ingestion...")
 
     api = KaggleApi()
     api.authenticate()
 
     os.makedirs("data/raw", exist_ok=True)
 
-    api.dataset_download_files(DATASET, path="data/raw", unzip=True)
+    # download + unzip
+    api.dataset_download_files(
+        DATASET,
+        path="data/raw",
+        unzip=True
+    )
 
     print("Download finished")
 
-    # lire le CSV automatiquement
-    files = [f for f in os.listdir("data/raw") if f.endswith(".csv")]
-    
-    if not files:
-        raise Exception("No CSV found after download")
-
-    df = pd.read_csv(f"data/raw/{files[0]}")
-
-    print("Dataset shape:", df.shape)
+    files = os.listdir("data/raw")
+    print("Files:", files)
 
 if __name__ == "__main__":
     download()
